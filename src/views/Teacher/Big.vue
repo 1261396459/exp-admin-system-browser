f03fb98b557458c381d1e50af21690225adc69d3
alsoeasy <1261396459@qq.com>
2021-08-07 13:54:59 +0800

    完成 任课老师-我的实验大课 页面布局和逻辑事件

--- src/views/Teacher/Big.vue
+++ src/views/Teacher/Big.vue
@@ -1,24 +1,77 @@
 <template>
-  <div>
-    实验大课
-  </div>
+  <el-card class="box-card" v-for="item,index in list" :key="index" @click="gotoSmall(item.CID,item.CNAME,item.CNUM)">
+    <el-row>
+      <el-col :span="20">
+        <el-row>
+          <el-col :span="18">{{ item.CNAME }}</el-col>
+          <el-col :span="6">{{ item.CNUM }}</el-col>
+        </el-row>
+        <el-row class="fit-text-margin">
+          <el-col :span="18">{{ item.CLNs }}</el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="4" class="ver-center">
+        <el-icon-right></el-icon-right>
+      </el-col>
+    </el-row>
+  </el-card>
+  <el-button class="setExp" type="primary" icon="el-icon-plus" circle @click="gotoSetExp"></el-button>
 </template>
 
 <script>
+  import teacher from '../../api/teacher.js'
 export default {
   data() {
     return {
-      
+      uid: this.$store.state.token.uid,
+      list: []
     }
   },
   methods: {
-    
+    async _initList(){
+      const res = await teacher.getBigs(this.uid)
+      console.log(res)
+      if(res.status == 200){
+        this.list = res.data
+        return ;
+      }
+      this.$message.warning('无实验课')
+    },
+    gotoSetExp(){
+      this.$router.push({ 
+        path: '/teacher/set-exp'
+      })
+    },
+    gotoSmall(cid,cname,cnum){
+      this.$router.push({
+        path: '/teacher/small',
+        query: {
+          cid,
+          cname,
+          cnum
+        }
+      })
+    }
   },
   mounted() {
-    
+    this._initList()
   }
 }
 </script>
 
 <style scoped lang="less">
+  .box-card {
+    width: 80%;
+    border-radius: 20px;
+    margin-top: @itemMarginHeight;
+    background-color: @themeLightColor;
+  }
+  .ver-center {
+    .flexRowVerCenter();
+  }
+  .setExp {
+    position: fixed;
+    right: @baseMargin*3;
+    bottom: @baseMargin*3+@footerHeight;
+  }
 </style>
