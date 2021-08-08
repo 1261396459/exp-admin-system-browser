4bb5113d7a6b4751eb8e0bfc339f7ec5f2073ee6
alsoeasy <1261396459@qq.com>
2021-08-08 15:58:59 +0800

    修改资源获取url和网络部署url

--- src/api/base.js
+++ src/api/base.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const apiHost = "http://120.79.168.124:40000";
+// const apiHost = "http://120.79.168.124:30000";
+const localHost = "http://127.0.0.1:30000";
 
 export default axios.create({
-  baseURL: apiHost,
+  baseURL: localHost,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json"
